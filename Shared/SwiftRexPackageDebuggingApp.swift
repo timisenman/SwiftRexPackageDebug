de272d10eb4892ba7100a77ce8850d142f441460
Tim Isenman <tim.isenman@gmail.com>
2021-06-12 22:12:01 +0900

    Add Xcode project with SwiftRex Package and compiler error

---
+++ Shared/SwiftRexPackageDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRexPackageDebuggingApp.swift
+//  Shared
+//
+//  Created by Tim Isenman on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRexPackageDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
